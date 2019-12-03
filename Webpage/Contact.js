aaa46d18a31430e98d39ade146a6a9fe1036517f
Eesha Kirubaharan <ek14ts@brocku.ca>
2019-12-03 13:56:52 -0500

    Webpage Edit

---
+++ Webpage/Contact.js
@@ -0,0 +1,4 @@
+.document.getElementById("firstname").value;
+.document.getElementById("lastname").value;
+.document.getElementById("emailaddress").value;
+.document.getElementById("comments").value;
